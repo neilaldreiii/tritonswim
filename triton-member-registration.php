ff233e80d7532e3173443dfbf6661b46d860b2ee
neilaldrei <neilaldrei@icloud.com>
2018-10-06 16:27:34 +0800

    found bugs

--- triton-member-registration.php
+++ triton-member-registration.php
@@ -17,114 +17,79 @@
     <link href="https://fonts.googleapis.com/css?family=Open+Sans|Roboto" rel="stylesheet">
 </head>
 <body>
-    <?php
-        if(isset($_GET['verify'])) {
-            $code = $_GET['verify'];
+<div class="container">
+    <header class="header">
+        <h1>Triton Swimclub Members - Sign Up</h1>
+    </header>
+    <div class="register">
+        <div class="form-header">
+            <h2>Sign Up</h2>
+        </div>
+        <form action="<?php echo $current_file; ?>" method="POST">
+            <div class="form-container">
+                <div class="input-field">
+                    <input type="text" name="username" placeholder="Username">
+                </div>
+                <div class="input-field">
+                    <input type="password" name="password" placeholder="Password">
+                </div>
+                <div class="input-field">
+                    <input type="text" name="fname" placeholder="First Name">
+                </div>
+                <div class="input-field">
+                    <input type="text" name="lname" placeholder="Last Name">
+                </div>
+                <div class="form-action">
+                    <a href="triton.php">Sign in Instead</a>
+                    <input type="submit" value="Sign up">
+                </div>
+            </div>
+        </form>
+        <?php
+        if(isset($_POST['username'])
+        && isset($_POST['password'])
+        && isset($_POST['fname'])
+        && isset($_POST['lname'])) {
 
-            if(!empty($code)) {
+            echo $username = $_POST['usernane'];
+            echo $password = $_POST['password'];
+            echo $fname = $_POST['fname'];
+            echo $lname = $_POST['lname'];
 
-                $verify = "SELECT * FROM `verify` WHERE `veri_code`='".mysqli_real_escape_string($con, $code)."'";
-                if($verify_query = mysqli_query($con, $verify)) {
+            if(!empty($username) 
+            && !empty($password)
+            && !empty($fname)
+            && !empty($lname)) {
 
-                    $num_rows = mysqli_num_rows($verify_query);
-                    if($num_rows == 1) {
-                        
-                        $row = mysqli_fetch_array($verify_query);
-                        $code_id = $row['id'];
-                        $db_stats = $row['status'];
+                $update = "UPDATE `verify` SET `status` = 'in-use' WHERE `verify`.`id`='$code_id'";
+                if($up_query = mysqli_query($con, $update)) {
 
-                        if($db_stats == 'new') {
-                        ?>
+                    $add = "INSERT INTO `admin` VALUES('','".mysqli_real_escape_string($con, $username)."', '".mysqli_real_escape_string($con, $password)."','".mysqli_real_escape_string($con, $fname)."', '".mysqli_real_escape_string($con, $lname)."')";
+                    if($add_query = mysqli_query($con, $add)) {
 
-                        <div class="container">
-                            <header class="header">
-                                <h1>Triton Swimclub Members - Sign Up</h1>
-                            </header>
-                            <div class="register">
-                                <div class="form-header">
-                                    <h2>Sign Up</h2>
-                                </div>
-                                <form action="lobby.php" method="POST">
-                                    <div class="form-container">
-                                        <div class="input-field">
-                                            <input type="text" name="username" placeholder="Username">
-                                        </div>
-                                        <div class="input-field">
-                                            <input type="password" name="password" placeholder="Password">
-                                        </div>
-                                        <div class="input-field">
-                                            <input type="text" name="fname" placeholder="First Name">
-                                        </div>
-                                        <div class="input-field">
-                                            <input type="text" name="lname" placeholder="Last Name">
-                                        </div>
-                                        <div class="form-action">
-                                            <a href="triton.php">Sign in Instead</a>
-                                            <input type="submit" value="Sign up">
-                                        </div>
-                                    </div>
-                                </form>
-                                <?php
-                                if(isset($_POST['username'])
-                                && isset($_POST['password'])
-                                && isset($_POST['fname'])
-                                && isset($_POST['lname'])) {
+                        header("Location: lobby.php");
 
-                                    echo $username = $_POST['usernane'];
-                                    echo $password = $_POST['password'];
-                                    echo $fname = $_POST['fname'];
-                                    echo $lname = $_POST['lname'];
+                    } else {
 
-                                    if(!empty($usernmae) 
-                                    && !empty($password)
-                                    && !empty($fname)
-                                    && !empty($lname)) {
+                        header("Location: events.php");
+                        echo '<p class="error">Failed adding member</p>';
 
-                                        $update = "UPDATE `verify` SET `status` = 'in-use' WHERE `verify`.`id`='$code_id'";
-                                        if($up_query = mysqli_query($con, $update)) {
-
-                                            $add = "INSERT INTO `admin` VALUES('','".mysqli_real_escape_string($con, $username)."', '".mysqli_real_escape_string($con, $password)."','".mysqli_real_escape_string($con, $fname)."', '".mysqli_real_escape_string($con, $lname)."')";
-                                            if($add_query = mysqli_query($con, $add)) {
-
-                                                header("Location: lobby.php");
-
-                                            } else {
-
-                                                header("Location: events.php");
-                                                echo '<p class="error">Failed adding member</p>';
-
-                                            }
-                                        } else {
-
-                                            header("Location: gallery.php");
-                                            echo '<p class="error">Verification code failed</p>';
-
-                                        }
-                                    } else {
-
-                                        header("Location: athletes.php");
-                                        echo '<p class="error">All fields are required.</p>';
-
-                                    }
-                                }
-                                ?>
-                            </div>
-                        </div>
-                        <?php
-                        } elseif($db_stats == 'in-use') {
-
-                            echo '<p class="error">The code you entered has already been used</p>';
-
-                        }
-
-                    } elseif($num_rows == 0) {
-                        echo '<p class="error">The code you entered does not exist. </p>';
                     }
+                } else {
+
+                    header("Location: gallery.php");
+                    echo '<p class="error">Verification code failed</p>';
+
                 }
+            } else {
+
+                header("Location: athletes.php");
+                echo '<p class="error">All fields are required.</p>';
+
             }
-        } else {
-            echo '<h1 style="margin: auto; padding: 20px; text-align: center;">Error: 404 (Nothing to do here.)</h1>';
         }
-    ?>
+        ?>
+    </div>
+</div>
 </body>
 </html>
